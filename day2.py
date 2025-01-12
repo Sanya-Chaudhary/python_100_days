73bd074f06b643b656a456418517c4269381efe0
Sanya-Chaudhary <sanyachaudhary819@gmail.com>
2025-01-12 19:13:17 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,5 @@
+r = int(input("enter radius of circle:"))
+area =(3.14*r*r)
+circumference = (2*3.14*r)
+print("Area of cirle is",round(area,2),"sq cm")
+print("circumference of circle is ",round(circumference,2),"cm")
